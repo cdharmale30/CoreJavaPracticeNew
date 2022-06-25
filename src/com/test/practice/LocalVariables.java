2d1c3a5cfafdc2cca161adb0a222eabe9af451b6
cdharmale30 <cdharmale30@gmail.com>
2022-06-25 13:06:15 -0400

    Test new

--- src/com/test/practice/LocalVariables.java
+++ src/com/test/practice/LocalVariables.java
@@ -2,14 +2,18 @@ package com.test.practice;
 
 public class LocalVariables {
 	 public int instance1=11;
-	 public static int  Teststatic=44;
+	 public static int  Teststatic=44;  //static Variable 
 	public void testMethod() {
-		int  lc= 10;
-		
+		 int  lc= 13;
+		System.out.println("This is static variable"+lc);
 	}
 public static void main(String[] args) {
 	LocalVariables lv=new LocalVariables();
 	
 	System.out.println(lv.instance1);
+	System.out.println("This is static variable"+Teststatic); //called static variable using class name
+	System.out.println(LocalVariables.Teststatic);  //called static variable using class name
+	
+	lv.testMethod();
 }
 }
