7b9ad156f5f883288d8febe85b062c5132969db9
cdharmale30 <cdharmale30@gmail.com>
2022-06-25 13:43:06 -0400

    new

---
+++ src/com/test/practice/AdditionOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package com.test.practice;
+
+public class AdditionOfTwoNumbers {
+public static void main(String[] args) {
+	 int a= 10;
+	  int b=20;
+	   int c=30;
+	    System.out.println(a+b+c);
+}
+}
---
+++ src/com/test/practice/Narrowing.java
@@ -0,0 +1,11 @@
+package com.test.practice;
+
+public class Narrowing {
+	public static void main(String[] args) {
+		double d= 300.00;
+		System.out.println(d);
+		int i=(int) d;
+		System.out.println("Test i: "+i);
+	}
+
+}
