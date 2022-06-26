bcf7440d9664c084cbb9525e1109726344533f86
cdharmale30 <cdharmale30@gmail.com>
2022-06-26 09:05:32 -0400

    Test

---
+++ src/com/test/practice/operators/UnaryOperator.java
@@ -0,0 +1,18 @@
+package com.test.practice.operators;
+
+public class UnaryOperator {
+	static int a=90;
+public static void main(String[] args) {
+	System.out.println(++a);
+
+	System.out.println(a);
+	System.out.println(a++);
+	System.out.println(++a);
+
+	System.out.println(++a);
+	System.out.println(++a);
+	System.out.println(a++);
+
+
+    }
+}
