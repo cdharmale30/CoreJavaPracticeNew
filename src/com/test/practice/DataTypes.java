897ea38482b09f8ceba47107e487e973532276c6
cdharmale30 <cdharmale30@gmail.com>
2022-06-26 06:30:12 -0400

    New code uploaded

---
+++ src/com/test/practice/DataTypes.java
@@ -0,0 +1,20 @@
+package com.test.practice;
+
+public class DataTypes {
+public static void main(String[] args) {
+	short s=10;
+	int i =40;
+	 long l=90l;
+	 char ch ='A';
+	 float f= 55.6f;
+	 double d= 33.55;
+	 boolean b =true;
+	 System.out.println(s);
+	 System.out.println(i);
+	 System.out.println(l);
+	 System.out.println(ch);
+	 System.out.println(f);
+	 System.out.println(b);
+
+}
+}
