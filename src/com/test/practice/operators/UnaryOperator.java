920441f4d823e364e48121fc99dd2af8a2657d23
cdharmale30 <cdharmale30@gmail.com>
2022-06-26 09:58:52 -0400

    test'

--- src/com/test/practice/operators/UnaryOperator.java
+++ src/com/test/practice/operators/UnaryOperator.java
@@ -4,15 +4,11 @@ public class UnaryOperator {
 	static int a=90;
 public static void main(String[] args) {
 	System.out.println(++a);
-
 	System.out.println(a);
 	System.out.println(a++);
 	System.out.println(++a);
-
 	System.out.println(++a);
 	System.out.println(++a);
 	System.out.println(a++);
-
-
     }
 }
---
+++ src/com/test/practice/operators/UnaryPlus.java
@@ -0,0 +1,12 @@
+package com.test.practice.operators;
+
+public class UnaryPlus {
+	public static void main(String[] args) {
+		int x=+99;//Unary Plus
+
+		
+		
+		System.out.println(x);
+	}
+
+}
