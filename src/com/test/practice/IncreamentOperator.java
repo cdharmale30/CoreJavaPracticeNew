6ab2466d81bfc9eade2a65ccb0027c45003c8052
cdharmale30 <cdharmale30@gmail.com>
2022-06-28 02:22:44 -0400

    testArithmatic

--- src/com/test/practice/IncreamentOperator.java
+++ src/com/test/practice/IncreamentOperator.java
@@ -14,7 +14,10 @@ public static void main(String[] args) {
 	System.out.println(a++);
 	System.out.println(++a);
 
+	System.out.println(a);
 
+	System.out.println(a--);
+	System.out.println(++a);
 
 }
 }
---
+++ src/com/test/practice/operators/ArithmaticOperator.java
@@ -0,0 +1,9 @@
+package com.test.practice.operators;
+
+public class ArithmaticOperator {
+public static void main(String[] args) {
+	int a=15;
+	int b =10;
+	System.out.println(a%b);
+}
+}
