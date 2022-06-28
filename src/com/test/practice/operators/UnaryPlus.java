1356716b91513c6a92831326acb7c17ee5d6d5ec
cdharmale30 <cdharmale30@gmail.com>
2022-06-28 01:35:57 -0400

    New

--- src/com/test/practice/operators/UnaryPlus.java
+++ src/com/test/practice/operators/UnaryPlus.java
@@ -3,10 +3,11 @@ package com.test.practice.operators;
 public class UnaryPlus {
 	public static void main(String[] args) {
 		int x=+99;//Unary Plus
+		int y=-99;//Unary Minuss
 
-		
-		
 		System.out.println(x);
+		System.out.println(y);
+
 	}
 
 }
