cee0a68fe164c786867282f2d4905373d083325c
cdharmale30 <cdharmale30@gmail.com>
2022-06-28 12:37:12 -0400

    test

--- src/com/test/practice/operators/ArithmaticOperator.java
+++ src/com/test/practice/operators/ArithmaticOperator.java
@@ -4,6 +4,16 @@ public class ArithmaticOperator {
 public static void main(String[] args) {
 	int a=15;
 	int b =10;
+	
+	int c =45;
+
 	System.out.println(a%b);
+	System.out.println(10*10/5+3-1*4/2);  
+	
+	System.out.println(~2);
+
+	System.out.println(~-2);
+
+
 }
 }
