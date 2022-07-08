275ac541b8840f7aa028bf7b7ea69026f3b4ae30
cdharmale30 <cdharmale30@gmail.com>
2022-07-08 23:06:12 +0530

    test

---
+++ src/com/test/conditionalstatement/SwitchStatement.java
@@ -0,0 +1,23 @@
+package com.test.conditionalstatement;
+
+public class SwitchStatement {
+public static void main(String[] args) {
+	String City = "1merut";
+	if ( City == "Chg") {
+		
+		 System.out.println("name of City is Chg");
+	}else if (City == "merut") {
+		 System.out.println("name of City is Merut");
+
+}
+	else if (City == "Test") {
+		 System.out.println("name of City is Test");
+
+}
+	else{ 
+		 System.out.println("not matched with anyone");
+
+		
+		
+	}
+}}
